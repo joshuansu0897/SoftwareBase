e4c5039ebf5a8adeb9328bac3ef4e10aae1c44ba
Joshua Saucedo <joshuansu0897@gmail.com>
2018-03-05 18:19:51 -0700

    faltaron comentarios

--- Salon/PrintCadenas/PrintCadenas.asm
+++ Salon/PrintCadenas/PrintCadenas.asm
@@ -1,4 +1,4 @@
-; SPrint.asm
+; PrintCadenas.asm
 ; autor:Joshua Saucedo
 ; fecha:5/mar/2018
 ; Es un programa que imprime Cadenas de texto
@@ -12,11 +12,11 @@ section .text
     GLOBAL _start           ; declaramos el punto de inicio
 _start:
     mov rax, mensaje        ; cargamos el primer mensaje
-    call sprint
+    call sprint             ; mandamos a llamar al "metodo" que imprime
     mov rax, mensajeSegundo ; cargamos el segundo mensaje
-    call sprint
+    call sprint             ; mandamos a llamar al "metodo" que imprime
     mov rax, mensajeTres    ; cargamos el tercer mensaje
-    call sprint
+    call sprint             ; mandamos a llamar al "metodo" que imprime
     mov rax, 1              ; SYS_EXIT
     int 0x80                ; llamada al sistema
 
@@ -25,17 +25,17 @@ sprint:
     push rcx                ; guardamos rcx
     push rbx                ; guardamos rbx
     push rax                ; guardamos rax
-    call strlen
+    call strlen             ; mandamos a llamar al "metodo" que nos da el tamanio de la cadena
     mov rdx, rax            ; longitud del mensaje a rdx
-    pop rax
+    pop rax                 ; sacamos el valor de rax
     mov rcx, rax            ; movemos el mensaje a rcx
     mov rax, 4              ; sys_write
     mov rbx, 1              ; sys_out
-    int 0x80
-    pop rbx
-    pop rcx
-    pop rdx
-    ret
+    int 0x80                ; llamada al kernel
+    pop rbx                 ; sacamos el valor de rbx
+    pop rcx                 ; sacamos el valor de rcx
+    pop rdx                 ; sacamos el valor de rdx
+    ret                     ; el return, para regresar en la continuacion del codigo de donde fue invocado
 
 strlen:
     push rbx                ; guardamos el valor de rbx
@@ -48,6 +48,6 @@ sigcar:
     jmp sigcar              ; y saltamos otra vez a sigcar, para que se ejecute todo otra vez
 
 finalizar:
-    sub rax, rbx            ; restamos la direcciones de memoria
-    pop rbx
-    ret
+    sub rax, rbx            ; restamos la direcciones de memoria y se guarda en rax
+    pop rbx                 ; sacamos el valor de rbx
+    ret                     ; el return, para regresar en la continuacion del codigo de donde fue invocado
