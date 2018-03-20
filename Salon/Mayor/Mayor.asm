b00dab4658a34a14c5557bcb528d73cc4546219e
Joshua Saucedo <joshuansu0897@gmail.com>
2018-03-20 14:23:04 -0700

    ya quedo como funcion y en funciones basicas

--- Salon/Mayor/Mayor.asm
+++ Salon/Mayor/Mayor.asm
@@ -10,29 +10,26 @@ section .text
     GLOBAL _start
 
 _start:
-    pop ecx         ; el primer valor en el stack es el de argumentos
-    cmp ecx, 3      ; comparamos con 3
-    jl nomasArgs    ; si es menor a 3 vamos a "nomasArgs"
-    pop eax         ; quitamos la basura inicial
-    dec ecx         ; restamos por el quitado de basura
-    pop eax         ; sacamos el primer numero de los argumentos
-    call atoi       ; convertimos el primero numero a numero
+    pop ecx          ; el primer valor en el stack es el de argumentos
+    cmp ecx, 3       ; comparamos con 3
+    jl nomasArgs     ; si es menor a 3 vamos a "nomasArgs"
+    pop eax          ; quitamos la basura inicial
+    dec ecx          ; restamos por el quitado de basura
+    pop eax          ; sacamos el primer numero de los argumentos
+    call atoi        ; convertimos el primero numero a numero
+    mov ebx, eax     ; movemos el valor de eax a ebx
+    dec ecx          ; decrementamos ecx por el pop de arriba
 
-setearDatos:
-    mov ebx, eax    ; movemos el valor de eax a ebx
-    dec ecx         ; decrementamos ecx por el pop de arriba
-
-numeroMayor:
-    cmp ecx, 0h     ; comparamos con 0
-    jz nomasArgs    ; si es 0 vamos a "nomasArgs"
-    pop eax         ; tomamos el primer numero del argumento
-    call atoi       ; convertimos la cadena a enteros
-    cmp eax, ebx    ; comparamos eax con ebx
-    jg setearDatos  ; vamos a "setearDatos"
-    dec ecx         ; decrementamos ecx
-    jmp numeroMayor ; recursivo
+sigArg:
+    cmp ecx, 0h      ; comparamos con 0
+    jz nomasArgs     ; si es 0 vamos a "nomasArgs"
+    pop eax          ; tomamos el primer numero del argumento
+    dec ecx          ; decrementamos ecx
+    call atoi        ; convertimos la cadena a enteros
+    call numeroMayor ; llamamo la funcion "numeroMayor" si eax es mayor que ebx el valor se mueve a ebx
+    jmp sigArg       ; recursivo
 
 nomasArgs:
-    mov eax, ebx    ; movemos el valor de ebx a eax
-    call iprintLF   ; imprimimos el valor de eax
-    jmp salida      ; salimos del programa
+    mov eax, ebx     ; movemos el valor de ebx a eax
+    call iprintLF    ; imprimimos el valor de eax
+    jmp salida       ; salimos del programa
