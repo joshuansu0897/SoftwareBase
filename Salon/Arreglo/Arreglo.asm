91a1cbedaa0223abc4f1d3d66c29acd4837cfb07
Joshua Saucedo <joshuansu0897@gmail.com>
2018-04-11 14:32:48 -0700

    algunas cosas que olvide y el de PromedioArreglos

--- Salon/Arreglo/Arreglo.asm
+++ Salon/Arreglo/Arreglo.asm
@@ -7,7 +7,7 @@
 %include '../funciones_basicas.asm'
 
 segment .bss
-    array resb 50
+    array resb 2000
 
 section .text
     GLOBAL _start
@@ -19,14 +19,13 @@ _start:
     pop eax               ; quitamos la basura inicial
     dec ecx               ; restamos por el quitado de basura
     mov esi, array        ; movemos el array a esi
+    mov eax, 0            ; seteamos el valor de eax en 0
 
 sigArg:
     cmp ecx, 0h           ; comparamos con 0
-    jz nomasArgs          ; si es 0 vamos a "nomasArgs"
+    jz ciclo_impresion    ; si es 0 vamos a "ciclo_impresion"
     pop ebx               ; tomamos el primer numero del argumento
     dec ecx               ; decrementamos ecx
-    mov edx, 0            ; seteamos el valor de edx en 0
-    mov eax, 0            ; seteamos el valor de eax en 0
 
 ciclo:
     cmp byte[ebx+edx],0   ; comparamos el valor con 0
@@ -35,6 +34,13 @@ ciclo:
     mov [esi+edx],al      ; tomamos el tama;o base del array y le sumamos el contador y en esa posicion moveremos al
     inc edx               ; incrementamos el contador
     jmp ciclo             ; siguiente letra de la palabra
+
+ciclo_impresion:          ; CICLO DE IMPRESION
+  mov eax, [esi+edx]      ; traemos de array a imprimir
+  call sprint             ; imprimimos
+  dec edx                 ; decrementamos contador
+  cmp edx,0               ; llegamos a 0?
+  jne ciclo_impresion     ; ciclar en caso de no llegar a cero
 
 nomasArgs:
     jmp salida            ; salimos del programa
